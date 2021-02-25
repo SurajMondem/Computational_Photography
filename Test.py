1cd8bfb9930a3b449d8915615f48fdb57c5c67d7
SurajMondem <surajmondem@gmail.com>
2021-02-25 14:14:30 -0500

    Tested Image Concepts

--- Test.py
+++ Test.py
@@ -1,12 +1,29 @@
 import numpy as np
 import cv2
 
-# Read Image
-img = cv2.imread('Works_images\Capture_img.JPG')
 
-# Write Image
-cv2.imwrite('output.png', img)
+if __name__ == '__main__':
 
-# Display Image
-cv2.imshow('Output', img)
-cv2.waitKey(0)
+    # Read Image
+    img = cv2.imread(r'C:\Users\suraj\Desktop\CV\Test_Images\lena1.jpg')
+
+    print(img.shape)
+
+    cv2.imshow('Input', img)
+
+    row, col, color = img.shape
+
+    for i in range(row):
+        for j in range(col):
+            for k in range(color):
+                if img[i, j, k] != 0:
+                    img[i, j, k] = 255
+                else:
+                    img[i, j, k] = 0
+
+    # Write Image
+    # cv2.imwrite('output.png', img)
+
+    # Display Image
+    cv2.imshow('Output', img)
+    cv2.waitKey(0)
